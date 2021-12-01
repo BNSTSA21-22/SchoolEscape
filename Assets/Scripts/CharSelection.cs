8f581f64fa22769ac0ccf03f8375ba15465e61d2
SlickFromMars <WChurchill@bnsstudent.org>
2021-12-01 17:01:16 -0500

    thingythingy

---
+++ Assets/Scripts/CharSelection.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharSelection : MonoBehaviour
+{
+    //this is the variable stuff
+    public GameObject[] playerObjects;
+    public int selectedCharacter = 0;
+
+    public string gameScene = "Cafeteria";
+
+    private string selectedCharacterDataName = "Selected Character";
+
+    //some cool voids
+    private void HideAllCharacters()
+    {
+        foreach (GameObject g in playerObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void NextCharacter()
+    {
+        playerObjects[selectedCharacter].SetActive(false);
+        selectedCharacter++;
+        if (selectedCharacter >= playerObjects.Length)
+        {
+            selectedCharacter = 0;
+        }
+        playerObjects[selectedCharacter].SetActive(true);
+    }
+
+    public void PreviousCharacter()
+    {
+        playerObjects[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if (selectedCharacter >= playerObjects.Length)
+        {
+            selectedCharacter = playerObjects.Length-1;
+        }
+        playerObjects[selectedCharacter].SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        PlayerPrefs.SetInt(selectedCharacterDataName, selectedCharacter);
+        SceneManager.LoadScene(gameScene);
+    }
+
+    //startup sequence
+    void Start()
+    {
+
+        HideAllCharacters();
+
+        selectedCharacter = PlayerPrefs.GetInt(selectedCharacterDataName, 0);
+
+        playerObjects[selectedCharacter].SetActive(true);
+    }
+}
