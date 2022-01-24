1624642fafe2854daf29965ab2101736408931d8
SlickFromMars <minecraftsummer2017@gmail.com>
2022-01-24 16:47:34 -0500

    enjoy

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -7,12 +7,19 @@ public class GameControl : MonoBehaviour
 {
     public string menuScene = "Character Selection";
     private string selectedCharacterDataName = "Selected Character";
+
     int selectedCharacter;
+
     public GameObject playerObject;
+    public Transform PlayerStartPosition;
+
+    public GameObject[] characters;
+
     // Start is called before the first frame update
     void Start()
     {
-        selectedCharacter = PlayerPrefs.GetInt(selectedCharacterDataName,0);
-        //playerObject = Instantiate(characters[selectedCharacter],PlayerStartPosition.position,characters[selectedCharacter].transform.rotation);    
+        selectedCharacter = PlayerPrefs.GetInt(selectedCharacterDataName, 0);
+        Debug.Log(selectedCharacter);
+        playerObject = Instantiate(characters[selectedCharacter],PlayerStartPosition.position, characters[selectedCharacter].transform.rotation);    
     }
 }
