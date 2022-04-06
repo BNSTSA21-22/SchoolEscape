b2bab7d669828d884e1ab0535eef51156021fcd8
SlickFromMars <minecraftsummer2017@gmail.com>
2022-04-06 16:49:00 -0400

    stuff

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -2,12 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingScreen : MonoBehaviour
 {
     public int minTime = 4;
 
+    public Image enemy;
+    public Sprite[] spritelist;
+
+    private int newind = 1;
+
     void Start() {
+        switch(LoadingData.sceneToLoad) {
+            case "Character Selection":
+                newind = 0;
+                break;
+
+            case "CafeteriaCutscene":
+                newind = 0;
+                break;
+        }
+
+        Debug.Log("Index: " + newind);
+
+        enemy.sprite = spritelist[newind];
+
         Invoke("loadThePlace", minTime);
     }
 
